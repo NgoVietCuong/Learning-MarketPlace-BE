753248f7604a32a2b76ccdbdd60b39870e195f6a
Hoa Nguyen <ngocuong972000@gmail.com>
2024-04-08 16:08:12 +0700

    hotfix change port

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(process.env.APP_PORT || 4000);
 }
 bootstrap();
