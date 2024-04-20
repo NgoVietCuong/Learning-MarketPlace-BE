5dd50f81f556fe2ae63028a0c538fb4d36ef0da2
Hoa Nguyen <hangongon2000@gmail.com>
2024-04-20 22:18:46 +0700

    fix: username-constraints

--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,9 +1,10 @@
 import { Transform } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, NotContains } from 'class-validator';
 
 export class SignUpDto {
   @IsString()
   @IsNotEmpty()
+  @NotContains(' ')
   username: string;
 
   @IsEmail()
