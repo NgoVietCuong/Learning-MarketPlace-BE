6b78927828f66867df72c6749dd34aa4a9a973d6
Hoa Nguyen <hangongon2000@gmail.com>
2024-04-21 23:02:17 +0700

    feat: get instructor profile

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,11 +5,12 @@ import { User } from 'src/entities/user.entity';
 import { Role } from 'src/entities/role.entity';
 import { InstructorProfile } from 'src/entities/instructor-profile.entity';
 import { UserController } from './controllers/user.controller';
+import { InstructorController } from './controllers/instructor.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Role, InstructorProfile])],
   providers: [UserService],
   exports: [UserService],
-  controllers: [UserController],
+  controllers: [UserController, InstructorController],
 })
 export class UserModule {}
