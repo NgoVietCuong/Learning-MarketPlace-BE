0641cf902c9c77d41b6454ed43ece0850dfe1aea
Hoa Nguyen <hangongon2000@gmail.com>
2024-05-14 23:19:29 +0700

    feat: get my course list

--- src/modules/learning/learning.controller.ts
+++ src/modules/learning/learning.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AllowAccess } from 'src/app/decorators/allow-access';
 import { Roles } from 'src/app/enums/common.enum';
@@ -19,5 +19,11 @@ export class LearningController {
   @Post('enrollment')
   async enrollCourse(@Body() body: EnrollCourseDto, @User('id') id: number) {
     return this.learningService.enrollCourse(body, id);
+  }
+
+  @ApiOperation({ summary: 'Get my course list' })
+  @Get('my-courses')
+  async getMyCourseList(@User('id') id: number) {
+    return this.learningService.getMyCourseList(id);
   }
 }
