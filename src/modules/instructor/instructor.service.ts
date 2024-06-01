5683b425e48fa3322c84c99c6aeb4ff3d1d8125c
Hoa Nguyen <hangongon2000@gmail.com>
2024-06-01 23:48:41 +0700

    refactor instructor by slug

--- src/modules/instructor/instructor.service.ts
+++ src/modules/instructor/instructor.service.ts
@@ -5,10 +5,16 @@ import { BaseService } from '../base/base.service';
 import { InstructorProfile } from 'src/entities/instructor-profile.entity';
 import { ChangeInstructorPictureDto } from './dto/change-instructor-picture.dto';
 import { ChangeInstructorProfileDto } from './dto/change-instructor-profile.dto';
+import { Enrollment } from 'src/entities/enrollment.entity';
+import { CourseReviewService } from '../course-review/course-review.service';
 
 @Injectable()
 export class InstructorService extends BaseService {
-  constructor(@InjectRepository(InstructorProfile) private instructorProfileRepo: Repository<InstructorProfile>,) {
+  constructor(
+    private courseReviewService: CourseReviewService,
+    @InjectRepository(InstructorProfile) private instructorProfileRepo: Repository<InstructorProfile>,
+    @InjectRepository(Enrollment) private enrollmentRepo: Repository<Enrollment>,
+  ) {
     super();
   }
 
@@ -33,9 +39,22 @@ export class InstructorService extends BaseService {
     const profile = await this.instructorProfileRepo
       .createQueryBuilder('IP')
       .innerJoin('IP.user', 'U')
+      .leftJoinAndSelect('IP.courses', 'C')
       .where('IP.slug = :slug', { slug })
       .andWhere('U.isActive = :isActive', { isActive: true })
+      .andWhere('C.isPublished = :isPublished', { isPublished: true })
       .getOne();
-    return this.responseOk(profile);
+
+    const courses = profile.courses;
+    const courseIds = courses.map((c) => c.id);
+
+    const totalStudents = await this.enrollmentRepo
+      .createQueryBuilder('E')
+      .leftJoin('E.course', 'C')
+      .where('C.id IN (:...courseIds)', { courseIds })
+      .getCount();
+    const totalReviews = await this.courseReviewService.getTotalReviews(courseIds);
+    const averageRating = await this.courseReviewService.getAverageRating(courseIds);
+    return this.responseOk({ profile, totalStudents, totalReviews, averageRating: averageRating.rating });
   }
 }
