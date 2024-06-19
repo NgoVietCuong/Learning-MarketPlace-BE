99e891fac9aebef83285bce5922491be96463f75
Hoa Nguyen <ngocuong972000@gmail.com>
2024-06-19 17:09:52 +0700

    fix: update data return

--- src/app/constants/pagination-dto.constant.ts
+++ src/app/constants/pagination-dto.constant.ts
@@ -1,9 +1,11 @@
+import { Type } from 'class-transformer';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { constants } from './common.constant';
 
 export class PaginationDtoConstant {
   @ApiPropertyOptional({ default: constants.PAGINATION.LIMIT_DEFAULT })
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   @Max(1000)
@@ -11,6 +13,7 @@ export class PaginationDtoConstant {
   limit: number = constants.PAGINATION.LIMIT_DEFAULT;
 
   @ApiPropertyOptional({ default: constants.PAGINATION.PAGE_DEFAULT })
+  @Type(() => Number)
   @IsNumber()
   @Min(1)
   @IsOptional()
